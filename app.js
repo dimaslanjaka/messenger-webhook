3254b6ad33d2845a67aa7da0d9794e6cbda3133f
dimaslanjaka <dimaslanjaka@gmail.com>
2018-11-17 20:18:20 +0700

    Initial commit

--- app.js
+++ app.js
@@ -102,13 +102,13 @@ app.get('/webhook', (req, res) => {
 
 function handleMessage(sender_psid, received_message) {
   let response;
-  
+  console.log(received_message);
   // Checks if the message contains text
   if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `Hello "${{user_first_name}}".\n You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
